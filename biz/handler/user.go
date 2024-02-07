d3b5f23bedb3bbadfd5334bafb456fbfe305c08d
wangzhiqian <835118160@qq.com>
2024-02-07 23:13:07 +0800

    [王志乾] 1.鉴权 2.获取用户信息 3.修改用户货币

--- biz/handler/user.go
+++ biz/handler/user.go
@@ -2,12 +2,19 @@ package handler
 
 import (
 	"github.com/gin-gonic/gin"
+	middlewares "stock/biz/middlerware"
 	"stock/biz/service"
 )
 
 func UserRouter(r *gin.RouterGroup) {
 	r.POST("/login", service.UserLogin)
-	r.POST("/register", service.UserRegister)
-	r.POST("/del", service.UserDel)
 	r.POST("/get", service.UserGet)
+
+	admin := r.Group("admin")
+	admin.Use(middlewares.AdminCheck())
+	{
+		admin.POST("/del", service.UserDel)
+		admin.POST("/register", service.UserRegister)
+		admin.POST("/doluck", service.UserDoLuck)
+	}
 }
